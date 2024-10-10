0b8f2c6a74ed433a97a425fdfa57414ec879ac08
Fernando Maldonado <fherjo2000@icloud.com>
2024-10-10 14:29:17 -0500

    nose

--- src/types/serviciosTypes/serviciosSP.ts
+++ src/types/serviciosTypes/serviciosSP.ts
@@ -1,9 +1,9 @@
 interface Servicio {
-    name: string;
-    status: number;
-  }
-  
-  interface Servidor {
-    servidor: string;
-    servicios: Servicio[];
-  }
+  name: string;
+  status: number;
+}
+
+interface Servidor {
+  servidor: string;
+  servicios: Servicio[];
+}
