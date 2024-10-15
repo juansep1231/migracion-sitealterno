fc18f13cee83aa37c2289eefe34565ae3d8ebf00
Juan Posso <juan.posso@epn.edu.ec>
2024-10-15 15:23:19 -0500

    feat: add integration for fetching servers and hub

--- src/hooks/granjas/useGranjasSa.ts
+++ src/hooks/granjas/useGranjasSa.ts
@@ -1,17 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 import { GranjaDTOModel, GranjaStatusOnly } from "../../types/granjasTypes/granjasSA";
 import axios from "axios";
 import { toast } from "react-toastify";
+
 
 // Custom hook to handle server loading and Hub connection
 export const useGranjasSa = () => {
   const [servers, setServers] = useState<GranjaDTOModel[]>([]);
   const [count, setCount] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [hubConnection, setHubConnection] = useState<HubConnection | null>(
-    null
-  );
+  const hubConnectionRef = useRef<HubConnection | null>(null);
+  
 
   const changeClusterStateReload = (cluster: string, message: string): void => {
     setServers((prevServers) =>
@@ -19,10 +19,10 @@ export const useGranjasSa = () => {
         if (server.code === cluster) {
           let newStatus: GranjaStatusOnly["status"];
           const parsedStatus = parseInt(message, 10);
-          if (!isNaN(parsedStatus) && [0, 1, 2, 5].includes(parsedStatus)) {
+          if (!isNaN(parsedStatus) && [0, 1, 2, 5,3].includes(parsedStatus)) {
             newStatus = parsedStatus as GranjaStatusOnly["status"];
-          } else if (message === "Loading") {
-            newStatus = "Loading";
+          } else if (message === "Cargando") {
+            newStatus = "Cargando";
           } else {
             newStatus = 5;
           }
@@ -37,37 +37,52 @@ export const useGranjasSa = () => {
     );
   };
 
-  // Function to load servers
+  //Start all clusters 
+
+  const startAllClusters = async () => {
+    try {
+      const path = `${import.meta.env.VITE_API_BASE_URL_ADMINISTRACION}${import.meta.env.VITE_API_ENDPOINT_STARTALL_GRANJASSA}`;
+
+      // Update server states to "3" (starting) and prepare clusters array
+      const clusters = servers.map((server) => {
+        changeClusterStateReload(server.code, "3");
+        return { code: server.code };
+      });
+
+      // Make the POST request to start clusters
+      const response = await axios.post(
+        `${path}?command=start&command2=global`,
+        clusters
+      );
+
+      console.log("mensajeee",response.data.message);
+      if (response.data.message.includes("error")) {
+        throw new Error("Error al iniciar los clusters");
+      }
+    } catch (error: any) {
+      // Handle error
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "An error occurred while starting clusters.";
+      toast.error(errorMessage);
+
+      // Update server states to "5" (error state)
+      servers.forEach((server) => {
+        changeClusterStateReload(server.code, "5");
+      });
+    }
+  };
+
+
   const loadServersAsync = async (): Promise<GranjaDTOModel[]> => {
     const path = `${import.meta.env.VITE_API_BASE_URL_GRANJAS}${
       import.meta.env.VITE_API_ENDPOINT_GRANJASSA
     }`;
-    const hubpath = `${import.meta.env.VITE_API_BASE_URL_ADMINISTRACIONHUB}`;
-
-    const connection = new HubConnectionBuilder()
-      .withUrl(hubpath)
-      .withAutomaticReconnect()
-      .build();
-
-    connection.on("ReceiveStatus", (cluster: string, message: string) => {
-      const encodedMsg = `${cluster}: ${message}`;
-      const splittedMessage = message.split("\n").length;
-      console.log(splittedMessage);
-      console.log(encodedMsg);
-      changeClusterStateReload(cluster, message);
-    });
-
-    try {
-      await connection.start();
-      setHubConnection(connection); // Save the connection in state
-    } catch (error) {
-      console.log("Cannot initialize the hub");
-      toast.error("No se puede iniciar el hub");
-    }
-
+  
     try {
       const response = await axios.get(path);
-      return response.data; 
+      return response.data;
     } catch (error: any) {
       if (error.response) {
         toast.error(`Error: ${error.response.data}`);
@@ -80,6 +95,36 @@ export const useGranjasSa = () => {
     }
   };
 
+  // Function to get NLB status
+  const getNLBStatusAsync = async (loadedServers:GranjaDTOModel[]) => {
+    try {
+      const path = `${import.meta.env.VITE_API_BASE_URL_ADMINISTRACION}${import.meta.env.VITE_API_ENDPOINT_START_NLB_QUERY}`;
+console.log("ejecutandogetnlbstatus")
+      const clusters = loadedServers.map((server) => {
+        const serverCode = server.code; // Capture the code
+        changeClusterStateReload(serverCode, "3"); // Update state
+        return { code: serverCode }; // Use the captured code
+      });
+      console.log("clusters",clusters);
+      const response = await axios.post(`${path}?command=global`, clusters);
+
+
+      console.log("mensajeeeee",response.data.message);
+      toast.success("NLBs actualizadas");
+    } catch (error: any) {
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "An error occurred while getting NLB status.";
+      toast.error(errorMessage);
+
+      loadedServers.forEach((server) => {
+        changeClusterStateReload(server.code, "5");
+      });
+    }
+  };
+
+
   // useEffect to load servers and handle Hub connection
   useEffect(() => {
     const initialize = async () => {
@@ -87,11 +132,14 @@ export const useGranjasSa = () => {
       try {
         const loadedServers = await loadServersAsync();
         setServers(loadedServers);
-
+        console.log(loadedServers)
         if (loadedServers && loadedServers.length > 0) {
           setCount((prevCount) => prevCount + 1);
           toast.success("Servidores actualizados");
+           // Call getNLBStatusAsync after loading servers
+      await getNLBStatusAsync(loadedServers);
         }
+       
       } catch {
         setServers([]);
         toast.error("No se puede cargar los servidores");
@@ -101,12 +149,44 @@ export const useGranjasSa = () => {
     };
 
     initialize();
+   
+    
+  }, []);
+
+  useEffect(() => {
+    const hubpath = `${import.meta.env.VITE_API_BASE_URL_ADMINISTRACIONHUB}`;
+  
+    const connection = new HubConnectionBuilder()
+      .withUrl(hubpath)
+      .withAutomaticReconnect()
+      .build();
+  
+    connection.on("ReceiveStatus", (cluster: string, message: string) => {
+      const encodedMsg = `${cluster}: ${message}`;
+      const splittedMessage = message.split("\n").length;
+      console.log("splitted",splittedMessage);
+      console.log("encoded",encodedMsg);
+      changeClusterStateReload(cluster, message);
+    });
+  
+    connection
+      .start()
+      .then(() => {
+        console.log("Hub connection started");
+        hubConnectionRef.current = connection;
+      })
+      .catch((error) => {
+        console.log("Cannot initialize the hub", error);
+        toast.error("No se puede iniciar el hub");
+      });
+  
+    // Cleanup on unmount
     return () => {
-      if (hubConnection) {
-        hubConnection.stop();
+      if (hubConnectionRef.current) {
+        hubConnectionRef.current.stop();
       }
     };
-  }, [hubConnection]);
+  }, []);
 
-  return { servers, count, loading };
+  return { servers, count, loading, startAllClusters };
 };
