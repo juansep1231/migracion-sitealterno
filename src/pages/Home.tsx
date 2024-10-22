cc976e3cbf99a7f61e9a85c812e8e2fa0d2d35ab
Fernando Maldonado <fherjo2000@icloud.com>
2024-10-22 10:59:39 -0500

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import { Server, Activity, BarChart } from "lucide-react"
+import Logo from "../assets/LogoCompleto.png"
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-red-500"></h1>
+    <div className="container mx-auto flex flex-col w-full h-full rounded-2xl bg-gray-300 items-center justify-center text-black ">
+    <div className=" text-center justify-center items-center">
+     
+      <h1 className="text-5xl md:text-7xl ">
+        Sistema Del Site Alterno
+      </h1>
+      <p className="text-xl md:text-2xl  mb-8 ">
+        Control en tiempo real del estado y rendimiento de tu infraestructura
+      </p>
+      <div className="flex justify-center space-x-8 mb-12 animate-fade-in-up animation-delay-400">
+        <Server className="w-16 h-16 text-blue-400" />
+        <Activity className="w-16 h-16 text-green-400" />
+        <BarChart className="w-16 h-16 text-yellow-400" />
+      </div>
+      
     </div>
+  </div>
   );
 };
 
