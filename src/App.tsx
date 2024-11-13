97750cb4fdfd95620f39f28e53ea8c8b501c0c53
Juan Posso <juan.posso@epn.edu.ec>
2024-11-13 15:27:33 -0500

    fix: change toast close logijc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { ToastContainer } from "react-toastify";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import { MainRouter } from "./routes/MainRouter";
@@ -6,10 +5,8 @@ import { MainRouter } from "./routes/MainRouter";
 function App() {
   return (
     <>
-      <ToastContainer autoClose={2000} />
       <MainRouter />
     </>
   );
 }
-
 export default App;
--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -1,10 +1,13 @@
+// MainRouter.tsx
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import { homeRoutes } from "./homeroutes";
 import { serviciosRoutes } from "./serviciosRoutes";
 import { granjasRoutes } from "./granjasroutes";
-
 import { F5Routes } from "./F5produnetroutes";
 import { flushDnsroutes } from "./flushDNSroutes";
+
 const router = createBrowserRouter([
   ...homeRoutes,
   ...serviciosRoutes,
@@ -13,6 +16,16 @@ const router = createBrowserRouter([
   ...F5Routes,
 ]);
 
-export const MainRouter = () => {
-  return <RouterProvider router={router} />;
+//listens to route events and dismisses the toast
+router.subscribe(() => {
+  toast.dismiss();
+});
+
+export const MainRouter: React.FC = () => {
+  return (
+    <>
+      <ToastContainer autoClose={1000} newestOnTop />
+      <RouterProvider router={router} />
+    </>
+  );
 };
